bccb3fd0e98c3ef79b809e9877e4897c51a897eb
edenuis <edenuis.lua@ncs.com.sg>
2021-08-15 16:57:00 +0800

    Build angular project

---
+++ go2shop-app/src/app/common/cart/cart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'go2shop-cart',
+	templateUrl: './cart.component.html'
+})
+export class CartComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit(): void {
+	}
+
+}
---
+++ go2shop-app/src/app/common/payment/payment.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'go2shop-payment',
+  templateUrl: './payment.component.html',
+  styles: [
+  ]
+})
+export class PaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
