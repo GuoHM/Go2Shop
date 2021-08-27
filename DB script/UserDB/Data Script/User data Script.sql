209f13f81692adb7c06d0db876c1082bb15ebd10
Guo Haoming <haoming.guo@ncs.com.sg>
2021-08-27 21:28:28 +0800

    change user db, add user test data

---
+++ DB script/UserDB/Data Script/User data Script.sql
@@ -0,0 +1,2 @@
+INSERT INTO `userdb`.`tb_user` (`ID`, `CARD_NUMBER`, `NAME`, `EXPIRY`, `ADDRESS`, `CONTACT_DETAIL`) VALUES ('1', '1', 'haoming', '1', '1', '1');
+INSERT INTO `userdb`.`tb_user` (`ID`, `CARD_NUMBER`, `NAME`, `EXPIRY`, `ADDRESS`, `CONTACT_DETAIL`) VALUES ('2', '2', 'go2shop', '2', '2', '2');
--- DB script/UserDB/Table Script/User DB Script.sql
+++ DB script/UserDB/Table Script/User DB Script.sql
@@ -11,17 +11,6 @@ DROP SCHEMA IF EXISTS `userdb` ;
 CREATE SCHEMA IF NOT EXISTS `userdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `userdb` ;
 
--- -----------------------------------------------------
--- Table `userdb`.`tb_user_type`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `userdb`.`tb_user_type` ;
-CREATE TABLE IF NOT EXISTS `userdb`.`tb_user_type` (
-  `ID` INT NOT NULL,
-  `USER_TYPE` VARCHAR(25) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table `userdb`.`tb_user`
@@ -34,12 +23,7 @@ CREATE TABLE IF NOT EXISTS `userdb`.`tb_user` (
   `EXPIRY` VARCHAR(5) NOT NULL,
   `ADDRESS` VARCHAR(100) NOT NULL,
   `CONTACT_DETAIL` VARCHAR(8) NOT NULL,
-  `TB_USER_TYPE_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`, `TB_USER_TYPE_ID`),
-  INDEX `fk_TB_USER_TB_USER_TYPE_idx` (`TB_USER_TYPE_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_TB_USER_TB_USER_TYPE`
-    FOREIGN KEY (`TB_USER_TYPE_ID`)
-    REFERENCES `userdb`.`tb_user_type` (`ID`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
