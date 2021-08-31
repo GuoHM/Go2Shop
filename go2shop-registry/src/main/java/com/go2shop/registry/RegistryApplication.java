7bb3b2fc4942527b71592af22a0d13dcd8121c03
Lim Ming Yan <mingyan.lim@ncs.com.sg>
2021-08-31 17:31:48 +0800

    - Updated pom.xml & application.yml

--- go2shop-registry/src/main/java/com/go2shop/registry/RegistryApplication.java
+++ go2shop-registry/src/main/java/com/go2shop/registry/RegistryApplication.java
@@ -1,9 +1,12 @@
 package com.go2shop.registry;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryApplication {
