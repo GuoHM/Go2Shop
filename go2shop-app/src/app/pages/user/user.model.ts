93b3a9be78af0bf547fc7476c019a3bc8f46f723
Guo Haoming <haoming.guo@ncs.com.sg>
2021-09-01 21:21:12 +0800

    update FE model

--- go2shop-app/src/app/pages/user/user.model.ts
+++ go2shop-app/src/app/pages/user/user.model.ts
@@ -10,14 +10,44 @@ export class UserLogin implements IUserLogin {
   ) { }
 }
 
-export interface IUserRegister extends IUserLogin {
-    passwordRepeat?: string;
+export interface IUserRegister {
+  username?: string;
+  password?: string;
+  name?: string;
+  cardNumber?: string;
+  address?: string;
+  contactDetail?: string;
+  type?: string;
 }
 
 export class UserRegister implements IUserRegister {
   constructor(
     public username?: string,
     public password?: string,
-    public passwordRepeat?: string
+    public name?: string,
+    public cardNumber?: string,
+    public address?: string,
+    public contactDetail?: string,
+    public type?: string,
+  ) { }
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  cardNumber: string;
+  address: string;
+  contactDetail: string;
+  type: string;
+}
+
+export class User implements IUser {
+  constructor(
+    public id: number,
+    public name: string,
+    public cardNumber: string,
+    public address: string,
+    public contactDetail: string,
+    public type: string,
   ) { }
 }
