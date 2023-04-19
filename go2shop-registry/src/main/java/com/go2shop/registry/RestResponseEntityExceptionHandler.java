1c46e265fa4cfc47fb948344fa425339f13ec667
熊建文 <janvenyong@gmail.com>
2023-04-20 07:48:55 +0800

    Added global exception handling

---
+++ go2shop-registry/src/main/java/com/go2shop/registry/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.go2shop.registry;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	public RestResponseEntityExceptionHandler() {
+		super();
+	}
+
+	@ExceptionHandler(value = { Exception.class })
+	public ResponseEntity<Object> exception(Exception ex, WebRequest request) {
+		logger.error(request + ", UserId: " + request.getHeader("UserId"), ex);
+		final String bodyOfResponse = "Unexpected error";
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR,
+				request);
+	}
+}
