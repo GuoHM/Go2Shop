f2c63dfa86ee5148fa552f9963c37eb5dba67ceb
熊建文 <janvenyong@gmail.com>
2023-04-24 00:07:44 +0800

    Added encryption to datasource password

--- go2shop-auth-service/src/main/java/com/go2shop/authentication/AuthenticationApplication.java
+++ go2shop-auth-service/src/main/java/com/go2shop/authentication/AuthenticationApplication.java
@@ -6,11 +6,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableEncryptableProperties
 public class AuthenticationApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
--- go2shop-catalogue-service/src/main/java/com/go2shop/catalogue/CatalogueApplication.java
+++ go2shop-catalogue-service/src/main/java/com/go2shop/catalogue/CatalogueApplication.java
@@ -6,11 +6,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableEncryptableProperties
 public class CatalogueApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogueApplication.class, args);
 	}
